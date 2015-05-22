0e427e636720fb3c0da00c8922e8787d990a5294
swheatley <shaylee1@me.com>
2015-05-21 23:27:46 -0600

    Street fighter will move or not depending on mouse release.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+$('.ryu').mouseenter(function() {
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+
+	.mousedown(function() {
+		//play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		//animate hadouken to the right of the screen
+	});
+});
