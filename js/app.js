8840a31bb1a154609f6f066a990fc28911daee67
swheatley <shaylee1@me.com>
2015-05-28 00:01:01 -0600

    Ryu willstrike a pose keying x and fire hadouken while clicking the mouse.  Updates to HTML for Jquery Streetfighter title

--- js/app.js
+++ js/app.js
@@ -1,23 +1,56 @@
+
 $(document).ready(function() {
-$('.ryu').mouseenter(function() {
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
+    $('.ryu').mouseenter(function() {
+            $('.ryu-still').hide();
+            $('.ryu-cool').hide();
+            $('.ryu-ready').show();
+            
+        })
+    
+$(document).keydown(function(x) {
+  if (x.keyCode == 88) {
+    $('.ryu-ready').hide();
+    $('.ryu-cool').show();
+  }
 })
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
+    $(document).keyup(function(x){
+        if (x.keyCode ==88){
+            $('.ryu-cool').hide();
+            $('.ryu-ready').show();
 
-	.mouseup(function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	})
+        }
+    })	
+        .mouseleave(function() {
+            $('.ryu-ready').hide();
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        })
 
-	.mousedown(function() {
-		//play hadouken sound
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//animate hadouken to the right of the screen
-	});
+    .mousedown(function() {
+    	playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+                    {'left': '1020px'},
+                500,
+                function(){
+                    $(this).hide();
+                    $(this).css('left', '520px');
+                });
+    })
+
+    .mouseup(function() {
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').hide();
+        $('.ryu-ready').show();
+    });
+
 });
+
+function playHadouken(){
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+
